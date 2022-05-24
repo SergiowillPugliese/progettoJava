fd719200d136752f3d93d1ccc191e770d2ff059b
sergiowillpugliese <pugliese.sergio1987@hotmail.it>
2022-05-25 00:53:14 +0200

    signup

---
+++ com.restaurant/src/main/java/com/restaurant/constants/RestaurantConstants.java
@@ -0,0 +1,6 @@
+package com.restaurant.constants;
+
+public class RestaurantConstants {
+    public static final String SOMETHING_WENT_WRONG = "SOMETHING WENT WRONG.";
+    public static final String INVALID_DATA = "Invalid Data.";
+}
